a38111dd4c25d228866a9894221171055ea15e8a
Finch Kyle <aron.ngenok@gmail.com>
2025-10-06 20:57:34 +0300

    Create model destinatin

--- destinations/models.py
+++ destinations/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=100)
+    price = models.IntegerField()
+    offer = models.BooleanField(default=False)
+    available = models.BooleanField(default=True)
