b04c767884925527eb7fc25f4a6f0316cc9e2f67
Finch Kyle <aron.ngenok@gmail.com>
2025-10-07 16:55:29 +0300

    Create Order and OrderItem models

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contib.auth.models import User
+from django.contrib.auth.models import User
 from destinations.models import Destination
 
 # Create your models here.
