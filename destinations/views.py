54c00244b5eea9d79a6750070c57122707f39342
Finch Kyle <aron.ngenok@gmail.com>
2025-10-08 12:32:04 +0300

    create destination_list and destination_detail views

--- destinations/views.py
+++ destinations/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Destination
 
 # Create your views here.
+def destination_list(request):
+    destinations = Destination.objects.all()
+    return render(request, "list.html", {"destinations": destinations})
+
+
+def destination_detail(request, pk):
+    destination = get_object_or_404(Destination, pk=pk)
+    return render(request, "detail.html" {"destination": destination})
